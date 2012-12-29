a820d4ba8f8573134dfe3c9ce1f5ab56149dfbe7
Casear <casearchu@gmail.com>
2012-12-29 14:50:03 +0800

    animation.js

---
+++ WindowsApp.FreshAnimation/js/animation.js
@@ -0,0 +1,31 @@
+﻿function AnimationObjectManager(_canvasId,_projectId,callback) {
+    var projectId=_projectId;
+    var canvasId=_canvasId;
+    var animationName;
+    this.images = [];
+    this.images.push({ title: "sea", url: "/animation/pi/sea.png" });
+    this.images.push({ title: "sky", url: "/animation/pi/sky.png" });
+    this.images.push({ title: "ground", url: "/animation/pi/ground.png" });
+
+    var htSize = {
+        width: document.body.clientWidth,
+        height: document.body.clientHeight
+    };
+    oLayer = new collie.Layer({
+        width: htSize.width,
+        height: htSize.height
+    });
+
+    this.images.forEach(function (file) {
+        collie.ImageManager.addImage(file.title, file.url);
+    });
+    new collie.MovableObject({ backgroundImage: this.images[0].title }).addTo(oLayer);
+    collie.Renderer.addLayer(oLayer);
+    collie.Renderer.load(document.getElementById(canvasId));
+    collie.Renderer.start();
+    if (callback != null) {
+        callback();
+    }
+    this.add
+
+}
