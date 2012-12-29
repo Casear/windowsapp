b356d99867a691ecc2c337134060ff72bc025529
yyken <yyken@outlook.com>
2012-12-29 12:45:43 +0800

    menu function

---
+++ WindowsApp.FreshAnimation/js/menu.js
@@ -0,0 +1,26 @@
+﻿(function () {
+    "use strict";
+
+    var dataArray = [
+    { title: "Basic banana", text: "Low-fat frozen yogurt"  },
+    { title: "Banana blast", text: "Ice cream"},
+    { title: "Brilliant banana", text: "Frozen custard"},
+    { title: "Orange surprise", text: "Sherbet"},
+    { title: "Original orange", text: "Sherbet"},
+    { title: "Vanilla", text: "Ice cream"},
+    { title: "Very vanilla", text: "Frozen custard"},
+    { title: "Marvelous mint", text: "Gelato"},
+    { title: "Succulent strawberry", text: "Sorbet"}
+    ];
+
+    var dataList = new WinJS.Binding.List(dataArray);
+
+    // Create a namespace to make the data publicly
+    // accessible. 
+    var publicMembers =
+        {
+            itemList: dataList
+        };
+    WinJS.Namespace.define("DataExample", publicMembers);
+
+})();
